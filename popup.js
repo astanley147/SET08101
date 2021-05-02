232353df61217e694f07e943528a72164e69c180
astanley147 <78031908+astanley147@users.noreply.github.com>
2021-05-02 15:22:18 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+function manualFunction() {
+    var manual = document.getElementById("manualPopup");
+    manual.classList.toggle("show");
+  }
+
+function mapFunction() {
+    var map = document.getElementById("mapPopup");
+    map.classList.toggle("show");
+  }
